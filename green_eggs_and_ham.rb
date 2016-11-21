ae9f3828ee6f606fcd7beae171b26c0412c07e2f
Michael Grein <michaelgrein92@gmail.com>
2016-11-21 17:38:55 -0500

    Explorer Mode Complete

--- green_eggs_and_ham.rb
+++ green_eggs_and_ham.rb
@@ -7,4 +7,24 @@ class GreenEggsAndHam
   def word_count
     count = @words.length
   end
+
+  def sorted_unique_words
+    downcase_words = @words.map { |word| word.downcase }
+    unique_words = downcase_words.uniq
+    sorted_words = unique_words.sort
+  end
+
+  def number_of_words_shorter_than(number)
+    shorter_than_word = @words.select { |word| word.length < number }
+    unique_shorter_words = shorter_than_word.length
+  end
+
+  def longest_word
+    word_length = @words.max_by { |word| word.length}
+  end
+
+  def stanzas
+    @text.split("\n\n")
+  end
+
 end
